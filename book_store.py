400b370fcd6863a0622ec041867fe286319ba635
AdhamHaithamEid <adhamhaithameid@gmail.com>
2024-05-05 21:59:57 +0300

    added comments and simplified the syntax

--- book_store.py
+++ book_store.py
@@ -1,111 +1,120 @@
+# Define a base class for all items in the bookstore
 class Item:
     def __init__(self, title, author, price):
-        self._title = title
-        self._author = author
-        self._price = price
+        self.title = title  # Title of the item
+        self.author = author  # Author of the item
+        self.price = price  # Price of the item
 
+    # Update the details of the item
     def update_details(self, title=None, author=None, price=None):
         if title is not None:
-            self._title = title
+            self.title = title
         if author is not None:
-            self._author = author
+            self.author = author
         if price is not None:
-            self._price = price
+            self.price = price
 
+    # Return a string representation of the item
     def __str__(self):
-        return f"{self._title} by {self._author}, ${self._price:.2f}"
+        return f"{self.title} by {self.author}, ${self.price:.2f}"
 
-
-
+# Define a class for books, inheriting from Item
 class Book(Item):
     def __init__(self, title, author, price, ISBN, genre, number_of_pages):
         super().__init__(title, author, price)
-        self._ISBN = ISBN
-        self._genre = genre
-        self._number_of_pages = number_of_pages
+        self.ISBN = ISBN  # ISBN number of the book
+        self.genre = genre  # Genre of the book
+        self.number_of_pages = number_of_pages  # Number of pages in the book
 
+    # Update details specific to books
     def update_details(self, ISBN=None, genre=None, number_of_pages=None, **kwargs):
         super().update_details(**kwargs)
         if ISBN is not None:
-            self._ISBN = ISBN
+            self.ISBN = ISBN
         if genre is not None:
-            self._genre = genre
+            self.genre = genre
         if number_of_pages is not None:
-            self._number_of_pages = number_of_pages
+            self.number_of_pages = number_of_pages
 
+    # Return a string representation of the book
     def __str__(self):
-        return f"{super().__str__()}, ISBN: {self._ISBN}, {self._genre}, {self._number_of_pages} pages"
+        return f"{super().__str__()}, ISBN: {self.ISBN}, Genre: {self.genre}, Pages: {self.number_of_pages}"
 
-
-
+# Define a class for magazines, inheriting from Item
 class Magazine(Item):
     def __init__(self, title, author, price, issue_number, publication_date, editor):
         super().__init__(title, author, price)
-        self._issue_number = issue_number
-        self._publication_date = publication_date
-        self._editor = editor
+        self.issue_number = issue_number  # Issue number of the magazine
+        self.publication_date = publication_date  # Publication date of the magazine
+        self.editor = editor  # Editor of the magazine
 
+    # Update details specific to magazines
     def update_details(self, issue_number=None, publication_date=None, editor=None, **kwargs):
         super().update_details(**kwargs)
         if issue_number is not None:
-            self._issue_number = issue_number
+            self.issue_number = issue_number
         if publication_date is not None:
-            self._publication_date = publication_date
+            self.publication_date = publication_date
         if editor is not None:
-            self._editor = editor
+            self.editor = editor
 
+    # Return a string representation of the magazine
     def __str__(self):
-        return f"{super().__str__()}, Issue {self._issue_number}, {self._publication_date}, Editor: {self._editor}"
+        return f"{super().__str__()}, Issue {self.issue_number}, Date: {self.publication_date}, Editor: {self.editor}"
 
-
-
+# Define a class for DVDs, inheriting from Item
 class DVD(Item):
     def __init__(self, title, author, price, director, duration, genre):
         super().__init__(title, author, price)
-        self._director = director
-        self._duration = duration
-        self._genre = genre
+        self.director = director  # Director of the DVD
+        self.duration = duration  # Duration of the DVD in minutes
+        self.genre = genre  # Genre of the DVD
 
+    # Update details specific to DVDs
     def update_details(self, director=None, duration=None, genre=None, **kwargs):
         super().update_details(**kwargs)
         if director is not None:
-            self._director = director
+            self.director = director
         if duration is not None:
-            self._duration = duration
+            self.duration = duration
         if genre is not None:
-            self._genre = genre
+            self.genre = genre
 
+    # Return a string representation of the DVD
     def __str__(self):
-        return f"{super().__str__()}, Directed by {self._director}, {self._duration} min, {self._genre}"
+        return f"{super().__str__()}, Directed by {self.director}, {self.duration} min, Genre: {self.genre}"
 
-
-
+# Define a class to manage the inventory of items
 class Inventory:
     def __init__(self):
-        self.items = []
+        self.items = []  # List to store items
 
+    # Add an item to the inventory
     def add_item(self, item):
         self.items.append(item)
 
+    # Remove an item from the inventory
     def remove_item(self, item):
         self.items.remove(item)
 
+    # Search for items in the inventory
     def search_item(self, search_query):
         results = []
         for item in self.items:
-            if search_query.lower() in item._title.lower() or search_query.lower() in item._author.lower():
+            if search_query.lower() in item.title.lower() or search_query.lower() in item.author.lower():
                 results.append(item)
         return results
 
+    # Print all items in the inventory
     def list_inventory(self):
         for item in self.items:
             print(item)
 
+    # Return a string representation of all items in the inventory
     def __str__(self):
         return "\n".join(str(item) for item in self.items)
 
-
-
+# Function to handle adding items to the inventory
 def add_item_to_inventory(inventory):
     print("Add a new item to the inventory.")
     item_type = input("Enter item type (book/magazine/dvd): ").lower()
@@ -135,6 +144,7 @@ def add_item_to_inventory(inventory):
     inventory.add_item(item)
     print("Item added successfully!")
 
+# Function to search for items in the inventory
 def search_inventory(inventory):
     query = input("Enter search query (title or author): ")
     results = inventory.search_item(query)
@@ -145,8 +155,7 @@ def search_inventory(inventory):
     else:
         print("No items found.")
 
-
-
+# Main function to drive the program
 def main():
     inventory = Inventory()
     while True:
